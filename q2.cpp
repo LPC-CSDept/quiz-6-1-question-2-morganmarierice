27e935756d23862a7533d106b5425d77d647f433
Adam Walder <walder2@llnl.gov>
2022-10-10 20:22:10 -0700

    first commit

--- q2.cpp
+++ q2.cpp
@@ -2,7 +2,18 @@
 #include <iomanip>
 using namespace std;
 
-// ******************************
+void getInput(int &, int &, int &);
+int findMin(int, int, int);
+void printResult(int, int, int, int);
+
+int main()
+{
+	int n1, n2, n3, min;
+	getInput(n1, n2, n3);
+	min = findMin(n1, n2, n3);
+	printResult(n1, n2, n3, min);
+	return(0);
+}// ******************************
 // Your code here
 // ******************************
 
@@ -11,4 +22,22 @@ using namespace std;
 void printResult(int n1, int n2, int n3, int min)
 {
 	cout << n1 << " " << n2 << " " << n3 << " " << min << " " << endl;
+}
+void getInput(int &n1, int &n2, int &n3)
+{
+	cout << "Enter 3 integers\n";
+	cin >> n1 >> n2 >> n3;
+}
+
+int findMin(int n1, int n2, int n3)
+{
+	int min = 0;
+	if((n1<n2) && (n1<n3))
+		min = n1;
+	else if((n2<n1) && (n2<n3))
+		min = n2;
+	else if((n3<n1) && (n3<n2))
+		min = n3;
+
+	return min;
 }
